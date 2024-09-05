376c5f60b9f62991ef90f5c8dcb7b773bb1391a4
Pranto Kumar <prantokumar.net@gmail.com>
2024-09-05 22:15:13 +0600

    new

---
+++ ROCK-PAPER-SCISSOR/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+
+int main(){
+  string UserInput;
+  string Ai = "rps";
+  cout<<"Welcome to Rock Paper Scissor Game."<<endl;
+  cout<<"Please enter you choice (Rock/Paper/Scissor->r/p/s) :";
+  cin>>UserInput;
+  system("pause");
+  return 0;
+}
